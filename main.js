e54f325189af4627598738209dbbd2d0c5b24ab9
Fabricio assuncao <fabricioassuncao855@outlook.com>
2022-07-06 12:15:20 -0300

    corrrigir algo aqui que nao era para existir

--- main.js
+++ main.js
@@ -82,6 +82,3 @@ function criaCalculadora() {
 const calculadora = criaCalculadora();
 calculadora.inicia();
 
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(json => console.log(json))
